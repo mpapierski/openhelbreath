306c07902e646865bad177069052f1b8bced3308
paavels <paavels@gmail.com>
2010-08-11 09:42:33 +0000

    Small formatting fixes and undefined main to compile in Windows. Also redirects stderr to stdout on Windows.

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,35 +1,43 @@
 #include "Game.h"
+#undef main
 
 #ifdef DEF_CONSOLE
-#include <io.h>
-#include <FCNTL.H>
+    #include <io.h>
+    #include <FCNTL.H>
 
-void ActivateConsole()
-{
-    AllocConsole();
+    void ActivateConsole()
+    {
+        AllocConsole();
 
-    HANDLE newConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
-    HANDLE newConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        HANDLE newConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+        HANDLE newConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	int inFd = _open_osfhandle((long) newConsoleInput, _O_TEXT);
-	int outFd = _open_osfhandle((long) newConsoleOutput, _O_TEXT);
+        int inFd = _open_osfhandle((long) newConsoleInput, _O_TEXT);
+        int outFd = _open_osfhandle((long) newConsoleOutput, _O_TEXT);
 
-	FILE* consoleIn = _fdopen(inFd, "r");
-	FILE* consoleOut = _fdopen(outFd, "w");
+        FILE* consoleIn = _fdopen(inFd, "r");
+        FILE* consoleOut = _fdopen(outFd, "w");
 
-	setvbuf(consoleIn, NULL, _IONBF, 0);
-	setvbuf(consoleOut, NULL, _IONBF, 0);
+        setvbuf(consoleIn, NULL, _IONBF, 0);
+        setvbuf(consoleOut, NULL, _IONBF, 0);
 
-	*stdin = *consoleIn;
-	*stdout = *consoleOut;
-}
+        *stdin = *consoleIn;
+        *stdout = *consoleOut;
+    }
 #endif
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-#ifdef DEF_CONSOLE
-	ActivateConsole();
-#endif
+    #ifdef DEF_CONSOLE
+        ActivateConsole();
+    #endif
 
-	return Game::getInstance().onExecute();
+    // TODO: Somehow on my computer output to stderr is not seen
+    // So I made this redirection. If intended output is going
+    // to be different, remove me
+    #ifdef WIN32
+        *stderr = *stdout;
+    #endif
+
+    return Game::getInstance().onExecute();
 }
